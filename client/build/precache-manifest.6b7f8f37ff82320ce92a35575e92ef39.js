8491ed8fdf8e4952694d3e2de620ccba788143f0
xtineroq <mcroque89@gmail.com>
2020-10-03 16:40:11 +1000

    deploying to heroku

---
+++ client/build/precache-manifest.6b7f8f37ff82320ce92a35575e92ef39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dbe9cbf2e3443c05b40bfa92244072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8228161b7ffa32d07c44",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea85c171612db0706583",
+    "url": "/static/js/2.61970590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.61970590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8228161b7ffa32d07c44",
+    "url": "/static/js/main.d7370572.chunk.js"
+  },
+  {
+    "revision": "41acd854cc4c094531a2",
+    "url": "/static/js/runtime-main.f119d467.js"
+  },
+  {
+    "revision": "bffb92b6d5248db82ca03be76341b4a6",
+    "url": "/static/media/book-bg.bffb92b6.jpg"
+  }
+]);
